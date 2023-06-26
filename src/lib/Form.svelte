df86dc7f14d72c38884152471cf9ef25b7be803a
Gilberto Espinoza <gilbertoespinoza97@gmail.com>
2023-06-25 21:39:25 -0700

    fetching latest news

---
+++ src/lib/Form.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let id;
+
+    const fetchNew = (async () => {
+        const response = await fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`);
+        return await response.json();
+    })()
+</script>
+
+{#await fetchNew}
+    <p>Esperando noticias..</p>
+{:then data} 
+    <article>
+        <h3>{data.title}</h3>  
+    </article>
+{/await}
